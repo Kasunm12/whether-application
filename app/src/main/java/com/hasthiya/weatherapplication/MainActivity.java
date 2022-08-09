d84f7daccbaeb15412c68d1cf27e2f66ea468015
Kasun M.Weerasekara <98303334+Kasunm12@users.noreply.github.com>
2022-08-09 17:54:36 +0530

    create  design ane api modal

--- app/src/main/java/com/hasthiya/weatherapplication/MainActivity.java
+++ app/src/main/java/com/hasthiya/weatherapplication/MainActivity.java
@@ -1,14 +1,50 @@
 package com.hasthiya.weatherapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RelativeLayout homeRL;
+    private ProgressBar loadingPB;
+    private TextView cityNameTV,temperatureTV,conditionTV;
+    private RecyclerView weatherRV;
+    private TextInputEditText cityEdt;
+    private ImageView backIV,iconIV,searchIV;
+
+    private ArrayList<WeatherRVModel> weatherRVModelArrayList;
+    private WeatherRVAdapter weatherRVAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        homeRL = findViewById(R.id.idRLHome);
+        loadingPB = findViewById(R.id.idPBLoading);
+        cityNameTV = findViewById(R.id.idTVCityName);
+        temperatureTV = findViewById(R.id.idTVTemperature);
+        conditionTV = findViewById(R.id.idTVCondition);
+        weatherRV = findViewById(R.id.idRvWeather);
+        cityEdt = findViewById(R.id.idEdtCity);
+        backIV = findViewById(R.id.idIVBack);
+        iconIV = findViewById(R.id.idIVIcon);
+        searchIV = findViewById(R.id.idIVSearch);
+
+        weatherRVModelArrayList = new ArrayList<>();
+        weatherRVAdapter = new WeatherRVAdapter(this,weatherRVModelArrayList);
+        weatherRV.setAdapter(weatherRVAdapter);
+
+
     }
 }
